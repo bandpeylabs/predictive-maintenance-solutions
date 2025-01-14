c135c824e326d294eb7f62c6ea26df5f1afc3779
RoozbehBandpey <RoozbehBandpey>
2025-01-14 17:46:12 +0000

    catalog creation

--- anomaly-detection/utils/generate-iot-data.py
+++ anomaly-detection/utils/generate-iot-data.py
@@ -1,9 +1,6 @@
 # Databricks notebook source
 # MAGIC %md
-# MAGIC
 # MAGIC # IoT Data Generation
-# MAGIC
-# MAGIC <img src="https://github.com/databricks-industry-solutions/iot-anomaly-detection/raw/main/resource/images/02_generate_iot_data.jpg">
 # MAGIC
 # MAGIC In this notebook, we use `dbldatagen` to generate fictitious data and push into a Kafka topic.
 # MAGIC
@@ -16,7 +13,7 @@
 # MAGIC - `sensor_1`
 # MAGIC - `sensor_2`
 # MAGIC - `sensor_3`
-# MAGIC - `us_state`
+# MAGIC - `site_name`
 # MAGIC
 # MAGIC where `sensor 1..3` are sensor values. 
 
@@ -35,13 +32,9 @@ import dbldatagen as dg
 import dbldatagen.distributions as dist
 from pyspark.sql.types import IntegerType, FloatType, StringType, LongType
 
-states = [ 'AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA',
-           'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME',
-           'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM',
-           'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX',
-           'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY' ]
+site_names = [ 'Luebeck', 'Luebeck-Travemuende', 'Falkenhagen', 'Ueberherrn' ]
 
-table_name = "iot_stream_example_"
+table_name = "iot_stream"
 spark.sql(f"drop table if exists {table_name}")
 
 data_rows = 2000
@@ -64,56 +57,8 @@ df_spec = (
   .withColumn("sensor_1", IntegerType(), minValue=-10, maxValue=100, random=True, distribution=dist.Gamma(40.0,9.0))
   .withColumn("sensor_2", IntegerType(), minValue=0, maxValue=10, random=True)
   .withColumn("sensor_3", FloatType(), minValue=0.0001, maxValue=1.0001, random=True)
-  .withColumn("state", StringType(), values=states, random=True)
+  .withColumn("site_name", StringType(), values=states, random=True)
 )
                             
 df = df_spec.build()
 display(df)
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC Write to Kafka
-
-# COMMAND ----------
-
-from pyspark.sql.functions import to_json, struct, col, cast
-from pyspark.sql.types import BinaryType
-import time
-
-#Get the data ready for Kafka
-kafka_ready_df = (
-                  df.select(
-                    col("id").cast(BinaryType()).alias("key"),
-                    to_json(
-                      struct(
-                        [col(column) for column in df.columns]
-                      )
-                    ).cast(BinaryType()).alias("value")
-                  )
-)
-
-display(kafka_ready_df)
-
-# COMMAND ----------
-
-options = {
-    "kafka.ssl.endpoint.identification.algorithm": "https",
-    "kafka.sasl.jaas.config": sasl_config,
-    "kafka.sasl.mechanism": sasl_mechanism,
-    "kafka.security.protocol" : security_protocol,
-    "kafka.bootstrap.servers": kafka_bootstrap_servers,
-    "group.id": 1,
-    "subscribe": topic,
-    "topic": topic,
-    "checkpointLocation": checkpoint_path
-}
-
-#Write to Kafka
-(
-  kafka_ready_df
-    .write
-    .format("kafka")
-    .options(**options)
-    .save()
-)
--- anomaly-detection/utils/notebook-config.py
+++ anomaly-detection/utils/notebook-config.py
@@ -1,20 +1,12 @@
 # Databricks notebook source
-# DBTITLE 1,Kafka config - see the RUNME notebook for instructions on setting up secrets
-# Either configure a secret scope containing the credentials for authenticating with Kafka, or replace with your own credentials
-
-kafka_bootstrap_servers = dbutils.secrets.get("predictive-maintenance", "iot-anomaly-kafka-bootstrap-server")
-security_protocol = "SASL_SSL"
-sasl_mechanism = "PLAIN"
-sasl_username = dbutils.secrets.get("predictive-maintenance", "iot-anomaly-sasl-username")
-sasl_password = dbutils.secrets.get("predictive-maintenance", "iot-anomaly-sasl-password")
-topic = "iot_msg_topic"
-sasl_config = f'org.apache.kafka.common.security.plain.PlainLoginModule required username="{sasl_username}" password="{sasl_password}";'
+# DBTITLE 1,Set database and streaming checkpoint
+checkpoint_path = "dbfs:/FileStore/demos/anomaly-detection/checkpoints"
+catalog = 'demos'
+database = "anomaly_detection"
 
 # COMMAND ----------
 
-# DBTITLE 1,Set database and streaming checkpoint
-checkpoint_path = "dbfs:/FileStore/demos/anomaly-detection/checkpoints" 
-database = "anomaly_detection"
+spark.sql(f"USE CATALOG {catalog}")
 
 # COMMAND ----------
 
@@ -31,5 +23,4 @@ spark.sql(f"create database if not exists {database}")
 # DBTITLE 1,mlflow settings
 import mlflow
 model_name = "iot_anomaly_detection"
-username = dbutils.notebook.entry_point.getDbutils().notebook().getContext().userName().get()
 mlflow.set_experiment('/demos/iot_anomaly_detection')
